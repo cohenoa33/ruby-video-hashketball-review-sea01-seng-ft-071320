015f4a848b7de627d7226ab7b455e902434ef2da
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-13 00:09:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,7 +196,7 @@ def big_shoe_rebounds
   game_hash.each do |team, team_data|
     team_data[:players].each  do |player| 
    if player[:shoe] > shoe_size
-     shoe_size = player{:shoe]
+     shoe_size = player[:shoe]
      rebounds= player[:rebounds]
     end
    end
